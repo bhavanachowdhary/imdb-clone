bccf491abb9cc089052b8174048ddfb3a02d2117
Rachit Desai <Rachit.Desai@RACHITDES-LT>
2023-07-10 14:45:41 +0530

    header-update

---
+++ src/Components/Header/Searchbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import './Searchbar.css'
+import logo from '../logo1.png'
+const Searchbar = () => {
+    
+    const [searchQuery, setSearchQuery] = useState('');
+    const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+    };
+
+    const handleSearchClick = () => {
+    // Perform search or API request with the searchQuery value
+    console.log('Perform search for:', searchQuery);
+    };
+
+    return (
+        <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search movies..."
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+        <div className="input-group-append">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={handleSearchClick}
+          >
+            Search
+          </button>
+        </div>
+        </div>
+  )
+}
+
+export default Searchbar
